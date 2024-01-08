15d9bf620c021488c5d5da51c44f75cafded969c
dbt-akshay <153073826+dbt-akshay@users.noreply.github.com>
2024-01-08 11:00:02 +0530

    Update streamlit_app.py
    fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit, pandas
 
+streamlit.title('My Parents Healthy New Diner')
+streamlit.header('Breakfast Menu')
+
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
