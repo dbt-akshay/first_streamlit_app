2922c07b25082246d4e87ac8c883d674321feb7c
dbt-akshay <153073826+dbt-akshay@users.noreply.github.com>
2024-01-08 10:51:02 +0530

    Update streamlit_app.py
    fixed header to text

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ import streamlit
 streamlit.title('My Healthy Diet')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & blueberry oatmeal')
-streamlit.header('Boiled eggs')
+streamlit.text('Boiled eggs')
