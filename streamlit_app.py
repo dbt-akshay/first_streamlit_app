0e9f797680e9c7efec4a08aa6107ad99b6073b0f
dbt-akshay <153073826+dbt-akshay@users.noreply.github.com>
2024-01-12 11:37:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,7 @@ def get_fruityvice_data(this_fruit_choice):
 # New section to display Fruityvice api response
 streamlit.header("View Our Fruit List - Add Your Favorites")
 try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information")
   else:
